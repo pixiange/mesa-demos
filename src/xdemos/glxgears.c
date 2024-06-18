017fe92ac7338af4b46d48208519c1a25109ad0a
Luc Ma <onion0709@gmail.com>
2024-06-18 22:51:53 +0800

    glxgears: use strtol instead of strtod
    Given that `samples` and `swapinterval` options are both
    integers, so no need to use strtod.
    
    Signed-off-by: Luc Ma <onion0709@gmail.com>

--- src/xdemos/glxgears.c
+++ src/xdemos/glxgears.c
@@ -815,11 +815,11 @@ main(int argc, char *argv[])
          stereo = GL_TRUE;
       }
       else if (i < argc - 1 && strcmp(argv[i], "-samples") == 0) {
-         samples = strtod(argv[i + 1], NULL);
+         samples = strtol(argv[i + 1], NULL, 10);
          ++i;
       }
       else if (i < argc - 1 && strcmp(argv[i], "-swapinterval") == 0) {
-         swapinterval = strtod(argv[i + 1], NULL);
+         swapinterval = strtol(argv[i + 1], NULL, 10);
          ++i;
       }
       else if (strcmp(argv[i], "-fullscreen") == 0) {
